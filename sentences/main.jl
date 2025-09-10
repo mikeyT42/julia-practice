321aaa6095327294a0d9990488c269e8cc77d61f
mikeyT42 <mrmojorisen0@gmail.com>
2025-09-10 01:46:30 -0400

    Getting started on the sentences program.

---
+++ sentences/main.jl
@@ -0,0 +1,21 @@
+@enum LoopControl again stop
+
+function input_loop()
+    println(
+        "\n\nPlease input a sentence. If you want to exit, just hit enter.\n")
+end
+
+################################################################################
+run(`clear`)
+
+println("---------------------------------------------------------------------")
+println("\tWelcome to the Sentence Data Aggregator")
+println("---------------------------------------------------------------------")
+println()
+
+
+println()
+println("---------------------------------------------------------------------")
+println("\t\t\t\tGoodbye")
+println("---------------------------------------------------------------------")
+
