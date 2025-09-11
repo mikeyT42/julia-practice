dbd9518945a107937199faf1ef4005f87665d19d
mikeyT42 <mrmojorisen0@gmail.com>
2025-09-11 02:19:21 -0400

    More code around input loop. Also, added keystrokes function.

--- sentences/main.jl
+++ sentences/main.jl
@@ -1,8 +1,19 @@
 @enum LoopControl again stop
 
+################################################################################
+function keystrokes(input)
+end
+
+################################################################################
 function input_loop()
     println(
         "\n\nPlease input a sentence. If you want to exit, just hit enter.\n")
+    input = readline()
+    if length(input) == 0
+        return stop
+    end
+
+    keys = keystrokes(input)
 end
 
 ################################################################################
@@ -11,8 +22,11 @@ run(`clear`)
 println("---------------------------------------------------------------------")
 println("\tWelcome to the Sentence Data Aggregator")
 println("---------------------------------------------------------------------")
-println()
 
+loop_control = again
+while loop_control != stop
+    global loop_control = input_loop()
+end
 
 println()
 println("---------------------------------------------------------------------")
