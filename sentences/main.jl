f7c0121f9516dcddf6380b5b5c1987a8beaacbcd
mikeyT42 <mrmojorisen0@gmail.com>
2025-09-12 02:13:47 -0400

    I added alpha_chars and digit_chars.

--- sentences/main.jl
+++ sentences/main.jl
@@ -12,6 +12,32 @@ function keystrokes(input)
 end
 
 ################################################################################
+function alpha_chars(input)
+    num_alpha = 0
+    for char in input
+        if !isletter(char)
+            continue
+        end
+
+        num_alpha += 1
+    end
+    return num_alpha
+end
+
+################################################################################
+function digit_chars(input)
+    num_digits = 0
+    for char in input
+        if !isdigit(char)
+            continue
+        end
+
+        num_digits += 1
+    end
+    return num_digits
+end
+
+################################################################################
 function input_loop()
     println(
         "\n\nPlease input a sentence. If you want to exit, just hit enter.\n")
@@ -22,6 +48,10 @@ function input_loop()
 
     total_keystrokes = keystrokes(input)
     @printf("\nKeystrokes: %10i", total_keystrokes);
+    total_alpha_chars = alpha_chars(input)
+    @printf("\nAlpha Characters: %4i", total_alpha_chars);
+    total_digit_chars = digit_chars(input)
+    @printf("\nNumeric Characters: %2i", total_digit_chars);
 end
 
 ################################################################################
